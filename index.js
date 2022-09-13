4a8d1e3c08930b3df5a967f6e1e697b2f66d72c1
viyan-najmaldin <najmadinviyan@gmail.com>
2022-09-13 19:22:27 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+console.log(mod);
+let max = 20;
+let min = 10;
+if (max > min) {
+  console.log(max);
+} else {
+  console.log(min);
+}
